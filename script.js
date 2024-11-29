9a66fe7c5c3ff00bcaf2956c14040c98add2cb2e
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-29 12:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let IntialText =document.getElementById('status');
+let IntialBtn =document.getElementById('enterBtn');
+
+IntialBtn.addEventListener('click', () =>
+	{
+IntialText.innerHTML = "<h1>Entered Metaverse</h1>";		
+});
+
